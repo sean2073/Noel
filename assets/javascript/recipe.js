054fa704ad4dabce9fa4c6270fa186c453ef01a8
sean2073 <thatbrowngurl@gmail.com>
2018-04-01 01:12:34 -0400

    added scroll-linked logic and added Edamam API

---
+++ assets/javascript/recipe.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $("#submit").on("click", function(event) {
+    var q = $("#query").val();
+    //var password = $("#password").val();
+    console.log(q);
+    var b = "beef";
+    //queryURL="https://api.edamam.com/search?q=" + q + "&app_id=${c24cde76}&app_key=${d8d15088e36d8a58570af6069ff5e5be}";
+    // Using Sy's api_id and api_key
+    queryURL =
+      "https://api.edamam.com/search?q=" +
+      q +
+      "&app_id=c24cde76&app_key=d8d15088e36d8a58570af6069ff5e5be";
+    // Using my api_id and api_key
+    queryURL2 =
+      "https://api.edamam.com/search?q=" +
+      q +
+      "&app_id=c69aa22c&app_key=ee3492ea77b67ed2322a011a5159e6b9";
+    $.ajax({
+      //$.curl({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log(response.hits[0].recipe.url);
+      console.log(response);
+      var res = response.hits[0].recipe.url;
+      console.log(res);
+      $("#recipe-content").append(res);
+    });
+    console.log("hello");
+  });
+});
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,34 +1,43 @@
 $(function() {
-    $("span:first").addClass("large-emphasize");
-    $("a").css({
-        "color": "#FFFFFF",
-        "text-decoration": "none",
-        "font-style": "none",
-        "margin-right": "35px"
-       // "box-shadow": "0 0 4px #811", 
-    });
-    $(".navLink").fadeTo(500,.60);
-  
-        
-    $(".navLink").hover(function() {
-        $(this).stop().fadeTo(500, .8);
-        $(this).css("text-decoration","underline"); 
-    }, 
+  $("span:first").addClass("large-emphasize");
+  $("a").css({
+    color: "#FFFFFF",
+    "text-decoration": "none",
+    "font-style": "none",
+    "margin-right": "35px"
+    // "box-shadow": "0 0 4px #811",
+  });
+  $(".navLink").fadeTo(500, 0.6);
+
+  $(".navLink").hover(
     function() {
-        $(this).stop().fadeTo(500, .6);
-        $(this).css("text-decoration","none"); 
-    });
-    $("#specialWords").css("color", "#c90909");
-        
-    
-   // $(".navLink #currentLink").css("color", "#ffffff");
-   // $(".navLink").not("#currentLink").css("color", "#ffffff").fadeTo(500, .8);
-   // $(".navLink").not("#currentLink").fadeTo(500, .8);
-    //$("li").not(":first").css("background-color", "rgba(180, 180, 30, 0.8)");
-   $("#submit-btn").css({
-       "background-color": "#435246",
-       "color": "#f6f7f4",
-       "border": "2px solid #c90909",
-       "font-size": "25px"
-   });
+      $(this)
+        .stop()
+        .fadeTo(500, 0.8);
+      $(this).css("text-decoration", "underline");
+    },
+    function() {
+      $(this)
+        .stop()
+        .fadeTo(500, 0.6);
+      $(this).css("text-decoration", "none");
+    }
+  );
+  $("#specialWords").css("color", "#c90909");
+
+  // $(".navLink #currentLink").css("color", "#ffffff");
+  // $(".navLink").not("#currentLink").css("color", "#ffffff").fadeTo(500, .8);
+  // $(".navLink").not("#currentLink").fadeTo(500, .8);
+  //$("li").not(":first").css("background-color", "rgba(180, 180, 30, 0.8)");
+  $("#submit-btn").css({
+    "background-color": "#435246",
+    color: "#f6f7f4",
+    border: "2px solid #c90909",
+    "font-size": "25px"
+  });
+
+//  $("#pic3").css("opacity", "0.3");
+ $("#recipe-content").css({
+    "background-color": "violet"
+  });
 });
